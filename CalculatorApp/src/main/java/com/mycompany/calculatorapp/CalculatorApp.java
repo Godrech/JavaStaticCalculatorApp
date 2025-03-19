6ff740a2f38fee86bc5b9f933ced48853f161853
Godrech <163859712+Godrech@users.noreply.github.com>
2025-03-19 14:15:40 +0200

    First Drag Push

---
+++ CalculatorApp/src/main/java/com/mycompany/calculatorapp/CalculatorApp.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculatorapp;
+
+/**
+ *
+ * @author ST10458246 Lukojiwa Godrech Lukoji
+ */
+public class CalculatorApp {
+
+    public static void main(String[] args) {
+        double num1= 64;
+        double num2 = 20;
+        
+        double answer1 = num1 + num2 ;
+        double answer2 = num1 - num2;
+        double answer3 = num1 / num2;
+        double answer4 = num1 * num2;
+        
+        
+        System.out.println(num1 + " + " + num2 +" = " + answer1);
+        System.out.println(num1 + " - " + num2 +" = " + answer2);
+        System.out.println(num1 + " / " + num2 +" = " + answer3);
+        System.out.println(num1 + " x " + num2 +" = " + answer4);
+        
+    }
+}
